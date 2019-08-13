4b542f2c1ce8b8ee8afb925f494263a5244b5fb5
MohamedYakout <mohamed.yakout91@gmail.com>
2019-08-13 11:52:36 +0200

    Add Transfer Logic

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,36 +1,19 @@
-require 'mini_magick'
 require 'open-uri'
 
 class Image < ApplicationRecord
+  has_many :transferred_images, dependent: :destroy
+
   has_attached_file :img, storage: :s3, path: "/progimg/:filename"
-  # Validate content type
-  # validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
-  # Validate filename
-  # validates_attachment_file_name :img, matches: [/png\z/, /jpe?g\z/, /bmp\z/, /tiff\z/]
-  # Explicitly do not validate
-  # do_not_validate_attachment_file_type :img
 
   validates_attachment_content_type :img, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
+  def img_url
+    self.img(:origin)
+  end
+
   before_save :set_image
 
-
-  #   https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
-
   def set_image
-    # minimagic_img = MiniMagick::Image.open(self.remote_url)
-    # self.img = minimagic_img.to_blob
-    # self.save!
-
-    # self.img = open(self.remote_url)
-    # self.save!
-
-    # self.img = URI.parse(self.remote_url)
-    # self.save!
-
     self.img = URI.parse(self.remote_url).open
-
-    # self.img = Paperclip.io_adapters.for(URI.parse(self.remote_url).to_s, { hash_digest: Digest::MD5 })
-    # self.save!
   end
 end
--- app/models/transferred_image.rb
+++ app/models/transferred_image.rb
@@ -1,3 +1,21 @@
+require 'mini_magick'
+require 'open-uri'
+
 class TransferredImage < ApplicationRecord
   belongs_to :image
+
+  has_attached_file :img, storage: :s3, path: "/tranferred_progimg/:filename"
+
+  validates_attachment_content_type :img, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  before_save :set_image
+
+  def set_image
+    minimagic_img = MiniMagick::Image.open(self.image.img_url)
+    minimagic_img.resize self.resize
+    minimagic_img.format self.format
+    minimagic_img.rotate self.rotate
+    minimagic_img.colorspace self.colorspace
+    self.img = File.open(minimagic_img.path)
+  end
 end
